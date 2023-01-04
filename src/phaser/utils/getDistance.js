9ac469e2bc7ebbaeb365dee96212240b28d68953
dirty g <you@example.com>
2023-01-04 13:21:51 +0200

    clean up

--- src/phaser/utils/getDistance.js
+++ src/phaser/utils/getDistance.js
@@ -1,3 +1,3 @@
-import { Math } from 'phaser'
+import { Between } from 'phaser/src/math/distance'
 
-export default (target1, target2) => Math.Distance.Between(target1.body.x, target1.body.y, target2.body.x, target2.body.y)
+export default (target1, target2) => Between(target1.body.x, target1.body.y, target2.body.x, target2.body.y)
